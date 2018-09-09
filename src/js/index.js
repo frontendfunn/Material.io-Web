4257507634ac41f574f90e62facbd81a1a7421b6
frontendfunn <frontendfunn@gmail.com>
2018-09-09 20:34:35 +0530

    02. Floating Action Button

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,9 @@
 require("../scss/style.scss");
 require("../index.html");
+import * as $ from "jquery/dist/jquery.min";
+
+$(document).ready(function() {
+  $(".toggler").on("click", function() {
+    $(".exitedBtn").toggleClass("mdc-fab--exited");
+  });
+});
