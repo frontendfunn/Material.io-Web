ac8e3655b9aa67e7e2a2663c4360725f3d61b838
frontendfunn <frontendfunn@gmail.com>
2018-09-14 23:58:11 +0530

    06. Chips

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
 require("../scss/style.scss");
 require("../index.html");
+
+import { MDCChip, MDCChipSet } from "@material/chips";
+import * as $ from "jquery/dist/jquery.min";
+
+$(document).ready(function() {
+  $(".mdc-chip-set").each(function(index, element) {
+    new MDCChipSet(element);
+  });
+});
