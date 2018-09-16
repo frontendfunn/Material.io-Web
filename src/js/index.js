edac5aa742766f8e87ae728cdcdb8e0d4bb01ef3
frontendfunn <frontendfunn@gmail.com>
2018-09-16 20:51:49 +0530

    07. Dialogs

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,20 @@
 require("../scss/style.scss");
 require("../index.html");
+
+import { MDCDialog } from "@material/dialog";
+import * as $ from "jquery/dist/jquery.min";
+$(document).ready(function() {
+  $(".block").each(function(index, blockElement) {
+    let showDialogBtn = $(blockElement).find(".showDialogBtn");
+    let dialog = new MDCDialog($(blockElement).find(".mdc-dialog")[0]);
+    showDialogBtn.on("click", function() {
+      dialog.show();
+    });
+    dialog.listen("MDCDialog:accept", function() {
+      console.log("Accepted");
+    });
+    dialog.listen("MDCDialog:cancel", function() {
+      console.log("Cancelled");
+    });
+  });
+});
