975687a4e317ca9b54bf785c6f6edde4e8b9a45f
frontendfunn <frontendfunn@gmail.com>
2018-09-26 13:06:12 +0530

    10.1 Checkboxes

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,14 @@
 require("../scss/style.scss");
 require("../index.html");
+
+import { MDCFormField } from "@material/form-field";
+import { MDCCheckbox } from "@material/checkbox";
+import * as $ from "jquery/dist/jquery.min";
+
+$(document).ready(function() {
+  $(".mdc-form-field").each(function(index, formFieldEle) {
+    let checkbox = new MDCCheckbox($(formFieldEle).find(".mdc-checkbox")[0]);
+    let formField = new MDCFormField(formFieldEle);
+    formField.input = checkbox;
+  });
+});
