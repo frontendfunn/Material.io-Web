b023a2d8ab811cde48e2b0a3e9b12a9c46e742fb
frontendfunn <frontendfunn@gmail.com>
2018-09-28 12:07:47 +0530

    10_2.TextField

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,20 @@
 require("../scss/style.scss");
 require("../index.html");
+
+import { MDCFloatingLabel } from "@material/floating-label";
+import { MDCLineRipple } from "@material/line-ripple";
+import { MDCTextField } from "@material/textfield";
+import { MDCNotchedOutline } from "@material/notched-outline";
+import * as $ from "jquery/dist/jquery.min";
+
+$(document).ready(function() {
+  $(".mdc-text-field").each(function(index, textField) {
+    new MDCTextField(textField);
+    let floatingLabel = $(textField).find(".mdc-floating-label")[0];
+    let lineRipple = $(textField).find(".mdc-line-ripple")[0];
+    let notchedOutline = $(textField).find(".mdc-noutched-outline")[0];
+    if (floatingLabel) new MDCFloatingLabel(floatingLabel);
+    if (lineRipple) new MDCLineRipple(lineRipple);
+    if (notchedOutline) new MDCNotchedOutline(notchedOutline);
+  });
+});
