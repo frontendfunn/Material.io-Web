da56b1be836c36c3421de16dc5a7c8d38dc71f7f
frontendfunn <frontendfunn@gmail.com>
2018-10-06 13:51:24 +0530

    local commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,14 @@
 require("../scss/style.scss");
 require("../index.html");
+
+import { MDCFormField } from "@material/form-field";
+import { MDCRadio } from "@material/radio";
+import * as $ from "jquery/dist/jquery.min";
+
+$(document).ready(function() {
+  $(".mdc-form-field").each(function(index, formFieldEle) {
+    let formField = new MDCFormField(formFieldEle);
+    let radio = new MDCRadio($(formFieldEle).find(".mdc-radio")[0]);
+    formField.input = radio;
+  });
+});
