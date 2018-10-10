fd4de72afc2186313465f6200f1f6f11a584ed90
frontendfunn <frontendfunn@gmail.com>
2018-10-10 14:09:51 +0530

    selet menu using vanilla js

--- src/js/index.js
+++ src/js/index.js
@@ -6,20 +6,32 @@ import { MDCNotchedOutline } from "@material/notched-outline";
 import { MDCSelect } from "@material/select";
 import * as $ from "jquery/dist/jquery.min";
 
-$(document).ready(function() {
-  $(".mdc-select").each(function(index, selectMenu) {
-    let floatingLabel = $(selectMenu).find(".mdc-floating-label")[0];
-    let lineRipple = $(selectMenu).find(".mdc-line-ripple")[0];
-    let notchedOutline = $(selectMenu).find(".mdc-notched-outline")[0];
-    const select = new MDCSelect(selectMenu);
+let mdcSelectMenuList = document.querySelectorAll(".mdc-select");
 
-    if (floatingLabel) new MDCFloatingLabel(floatingLabel);
-    if (lineRipple) new MDCLineRipple(lineRipple);
-    if (notchedOutline) new MDCNotchedOutline(notchedOutline);
-    select.listen("change", () => {
-      console.log(
-        `You Selected ${select.selectedIndex} and its value is ${select.value}`
-      );
-    });
+for (let selectMenuEle of mdcSelectMenuList) {
+  const select = new MDCSelect(selectMenuEle);
+
+  select.listen("change", () => {
+    console.log(
+      `You Selected ${select.selectedIndex} and its value is ${select.value}`
+    );
   });
-});
+}
+
+// $(document).ready(function() {
+//   $(".mdc-select").each(function(index, selectMenu) {
+//     let floatingLabel = $(selectMenu).find(".mdc-floating-label")[0];
+//     let lineRipple = $(selectMenu).find(".mdc-line-ripple")[0];
+//     let notchedOutline = $(selectMenu).find(".mdc-notched-outline")[0];
+//     const select = new MDCSelect(selectMenu);
+
+//     if (floatingLabel) new MDCFloatingLabel(floatingLabel);
+//     if (lineRipple) new MDCLineRipple(lineRipple);
+//     if (notchedOutline) new MDCNotchedOutline(notchedOutline);
+//     select.listen("change", () => {
+//       console.log(
+//         `You Selected ${select.selectedIndex} and its value is ${select.value}`
+//       );
+//     });
+//   });
+// });
